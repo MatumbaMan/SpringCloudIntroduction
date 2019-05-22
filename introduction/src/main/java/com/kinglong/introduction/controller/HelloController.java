6c029fc06e92f5b6fae9c3cbc597391581735405
houjinlong <houjinlong@cardioray.com.cn>
2019-05-22 16:45:06 +0800

    环境配置

--- introduction/src/main/java/com/kinglong/introduction/controller/HelloController.java
+++ introduction/src/main/java/com/kinglong/introduction/controller/HelloController.java
@@ -1,13 +1,17 @@
 package com.kinglong.introduction.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${book.name}")
+    private String bookName;
+
     @RequestMapping("/hello")
     public String hello() {
-        return "hello, king";
+        return "hello, king. 自定义参数为 " + bookName;
     }
 }

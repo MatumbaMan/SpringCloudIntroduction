34031903cfcb72c8237e06b97c16c16f33decde0
houjinlong <houjinlong@cardioray.com.cn>
2019-05-27 16:43:27 +0800

    增加hystrix

---
+++ 3.hystrix/ribbon_consumer/src/main/java/com/kinglong/ribbon_consumer/controller/RibbonController.java
@@ -0,0 +1,20 @@
+package com.kinglong.ribbon_consumer.controller;
+
+import com.kinglong.ribbon_consumer.HelloService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RibbonController {
+
+    @Autowired
+    HelloService helloService;
+
+    @RequestMapping(value = "/ribbon-consumer", method = RequestMethod.GET)
+    public String helloConsumer() {
+        return helloService.helloService();
+    }
+}
